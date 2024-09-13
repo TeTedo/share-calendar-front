7c72309f3edfe36269249b534d31405c1557a8b1
박태석 <qkrxotjr9508@gmail.com>
2024-09-13 17:28:41 +0900

    chore : bottom nav dummy json

--- src/components/bottomNav/BottomNav.tsx
+++ src/components/bottomNav/BottomNav.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { BottomNavWrapper } from "./BottomNav.style";
+import BottomNavJson from "./BottomNav.json";
 
 export const BottomNav = () => {
-  return <BottomNavWrapper>BottomNav</BottomNavWrapper>;
+  return (
+    <BottomNavWrapper>
+      {BottomNavJson.list
+        .sort((a, b) => a.index - b.index)
+        .map((menu) => (
+          <div key={menu.id}>
+            <img src={menu.iconUrl} alt={menu.iconAlt} />
+          </div>
+        ))}
+    </BottomNavWrapper>
+  );
 };
