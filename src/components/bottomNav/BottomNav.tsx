1352f7a8ba15115acfbaadc677f9b709e23f18ba
박태석 <qkrxotjr9508@gmail.com>
2024-09-13 17:33:43 +0900

    chore : bottom nav onClick navigation

--- src/components/bottomNav/BottomNav.tsx
+++ src/components/bottomNav/BottomNav.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { BottomNavWrapper } from "./BottomNav.style";
 import BottomNavJson from "./BottomNav.json";
+import { useNavigate } from "react-router-dom";
 
 export const BottomNav = () => {
+  const nav = useNavigate();
+
+  const navHandler = (url: string) => {
+    nav(url);
+  };
   return (
     <BottomNavWrapper>
       {BottomNavJson.list
         .sort((a, b) => a.index - b.index)
         .map((menu) => (
-          <div key={menu.id}>
+          <div
+            key={menu.id}
+            onClick={(e) => {
+              e.preventDefault();
+              navHandler(menu.navUrl);
+            }}
+          >
             <img src={menu.iconUrl} alt={menu.iconAlt} />
           </div>
         ))}
