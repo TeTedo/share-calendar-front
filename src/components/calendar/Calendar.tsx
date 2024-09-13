7f7c0ca4681833c76e570bf0121864f0d71609d5
박태석 <qkrxotjr9508@gmail.com>
2024-09-13 18:38:54 +0900

    feat : calendar

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { CalendarWrapper } from "./Calendar.style";
+
+export const Calendar = () => {
+  return <CalendarWrapper>Calendar</CalendarWrapper>;
+};
---
+++ src/components/calendar/CalendarDetail.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CalendarDetailWrapper } from "./CalendarDetail.style";
+
+interface CalendarDetailProp {
+  day: number;
+  color: string; // #aaaaaa  형식으로 # 포함한 6 hex
+  backgroundColor: string; // #aaaaaa  형식으로 # 포함한 6 hex
+  fontSize: string; // 10rem 형식으로 rem 포함한 string
+  list: any[]; // TODO 안에 들어갈 목록 타입 정해지면 타입 교체
+}
+
+export const CalendarDetail = (props: CalendarDetailProp) => {
+  return <CalendarDetailWrapper></CalendarDetailWrapper>;
+};
