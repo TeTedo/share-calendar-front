eca97a5e41d55d09e3e7d59e61e87e962b1eb078
박태석 <qkrxotjr9508@gmail.com>
2024-10-04 19:56:21 +0900

    feat : recoil 변수 저장 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { TanstackQueryProvider } from "provider/TanstackQueryProvider";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import { Router } from "router/Router";
 
 function App() {
   return (
     <div>
-      <TanstackQueryProvider>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </TanstackQueryProvider>
+      <RecoilRoot>
+        <TanstackQueryProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </TanstackQueryProvider>
+      </RecoilRoot>
     </div>
   );
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { memberState } from "state/recoil/memberState";
 
 export const HomePage = () => {
-  return <div>Home</div>;
+  const memberRecoilValue = useRecoilValue(memberState);
+
+  return (
+    <div>
+      <img src={memberRecoilValue.profileImg} alt="" />
+      <div>회원코드 : {memberRecoilValue.uuid}</div>
+      <div>{memberRecoilValue.name}님 안녕하세요!</div>
+    </div>
+  );
 };
