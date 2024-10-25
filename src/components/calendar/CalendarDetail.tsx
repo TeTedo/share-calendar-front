c43ab406195924df61ca6a31468de02abb016118
박태석 <qkrxotjr9508@gmail.com>
2024-10-25 14:37:28 +0900

    feat : 스케줄 추가

--- src/components/calendar/CalendarDetail.tsx
+++ src/components/calendar/CalendarDetail.tsx
@@ -9,7 +9,7 @@ interface CalendarDetailProp {
   color: string; // #aaaaaa  형식으로 # 포함한 6 hex
   backgroundColor: string; // #aaaaaa  형식으로 # 포함한 6 hex
   fontSize: string; // 10rem 형식으로 rem 포함한 string
-  scheduleList: IScheduleDetail[]; // TODO 안에 들어갈 목록 타입 정해지면 타입 교체
+  scheduleList: IScheduleDto[]; // TODO 안에 들어갈 목록 타입 정해지면 타입 교체
 }
 
 export const CalendarDetail = (props: CalendarDetailProp) => {
---
+++ src/hooks/api/schedule/useGetScheduleListByGroup.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const useGetScheduleListByGroup = () => {
+  return <div>useGetScheduleListByGroup</div>;
+};
